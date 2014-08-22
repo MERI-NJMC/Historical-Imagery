9eda9426eaa70311e8f9e855d1c7ef0f4fe82cbc
Steven Birkner <stevebirk36@gmail.com>
2014-08-22 11:05:46 -0400

    New function for setting the layer of the scroll spy.

--- js/main.js
+++ js/main.js
@@ -4,74 +4,77 @@ require([
   "esri/arcgis/utils",
   "dojo/_base/array",
   "esri/layers/layer",
+  "dojo/on",
+  "dojo/dom",
+  "dojo/query",
+  "dojo/dom-style",
+  "dijit/_WidgetBase",
   "dojo/domReady!"
-], function (Map, LayerSwipe, arcgisUtils, array, Layer) {
+], function (Map, LayerSwipe, arcgisUtils, array, Layer, on, dom, query, domStyle,_WidgetBase) {
+
+  var swipeWidget;
 
   //First string is the map ID from arcGIS online
   var mapDeferred = arcgisUtils.createMap("c5658cd5b8f6469fbfe9af4e678a3169", "map");
   mapDeferred.then(function(response) {
     
-    //initailizes the layer used in the scope widget. first controls whether the app is on its first go or not. var will equal 0 i
-    var initScope = function(lName,first) {
+
       var id,
           map = response.map,
-          title = lName;
-      
+          layers = response.itemInfo.itemData.operationalLayers,
+          layerIDs = [],
+          swipeLayer,
+          tempLayer;
       //loop through all the operational layers in the web map 
       //to find the one with the specified title;
-      var layers = response.itemInfo.itemData.operationalLayers;
-      var layerIDs = [];
-      array.some(layers, function(layer) {
-        layerIDs.push(layer.id);
-        if (layer.title === title) {
-          id = layer.id;
-          if(layer.featureCollection && layer.featureCollection.layers.length) {
-            id = layer.featureCollection.layers[0].id;
-          }
-        } else {
-
-          return false;
-
-        }
-      });
       
-      if (id) {
-        var swipeLayer = map.getLayer(id);
-        for (var i=0; i<layerIDs.length; i++) {
-          var tempLayer = map.getLayer(layerIDs[i]);
-          if (tempLayer != swipeLayer) {
-            tempLayer.hide();
+      setScope('IMG_2012_C');
+      function setScope(title){
+        layerIDs = [];
+        array.some(layers, function(layer) {
+              layerIDs.push(layer.id);
+              if (layer.title === title) {
+                id = layer.id;
+                if(layer.featureCollection && layer.featureCollection.layers.length) {
+                  id = layer.featureCollection.layers[0].id;
+                }
+              } else {
+      
+                return false;
+      
+              }
+            });
+            
+            if (id) {
+              swipeLayer = map.getLayer(id);
+              for (var i=0; i<layerIDs.length; i++) {
+                tempLayer = map.getLayer(layerIDs[i]);
+                if (tempLayer != swipeLayer) {
+                  tempLayer.hide();
+                } else {
+                  tempLayer.show();
+                }
+              }
           }
-        }
         
-        if(first) {
-          var swipeWidget = new LayerSwipe({
+        
+        var swipeWidget = new LayerSwipe({
             type: "scope",  //Try switching to "scope" or "horizontal"
             map: map,
-            layers: [swipeLayer]
+            layers: layerIDs
           }, "swipeDiv");
-          swipeWidget.startup();
-
-        } else {
-          swipeWidget.destroy();
-          swipeWidget = null;
-          swipeWidget = new LayerSwipe({
-            type: "scope",  //Try switching to "scope" or "horizontal"
-            map: map,
-            layers: [swipeLayer]
-          }, "swipeDiv");
-          swipeWidget.startup();
-        }
+        swipeWidget.startup();
+        console.log(swipeWidget);
+        
       }
 
-    };
-    //Sets the initial scope to be for 2012 imagery. base map imagery is 2014. 
-    initScope('IMG_2012_C',1);
-   
 
+
+  
     $(document).ready(function() {
       $('#brad').on('click', function() {
-        initScope('IMG_1930_BW',0);
+        setScope('IMG_1930_BW');
+
       });
     });
 
